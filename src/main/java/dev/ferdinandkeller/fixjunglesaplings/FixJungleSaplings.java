31b38c2341820bf91f50aab9317988ae3a39a8d3
Ferdinand Keller <ferdinand.keller@proton.me>
2025-08-30 16:57:26 +0200

    update logger text

--- src/main/java/dev/ferdinandkeller/fixjunglesaplings/FixJungleSaplings.java
+++ src/main/java/dev/ferdinandkeller/fixjunglesaplings/FixJungleSaplings.java
@@ -7,7 +7,7 @@ import org.slf4j.LoggerFactory;
 
 public class FixJungleSaplings implements ModInitializer {
 	public static final String MOD_ID = "fix-jungle-saplings";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final Logger LOGGER = LoggerFactory.getLogger("Fix Jungle Saplings");
 
 	@Override
 	public void onInitialize() {
